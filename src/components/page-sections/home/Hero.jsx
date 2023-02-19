722224f62b86749623ac5bc4ebebf32c8feae768
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-02-19 18:30:10 +0000

    Finished Hero section

--- src/components/page-sections/home/Hero.jsx
+++ src/components/page-sections/home/Hero.jsx
@@ -1,10 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
+import HeroImg from '../../images/patternpad.svg'
 
 function Hero() {
   return (
-    <main className='w-full h-[40vh] pt-[10vh]'>
-        <section className='max-w-[1600px] mx-auto h-full border m-3'>
-
+    <main className='w-full lg:h-[40vh] pt-[20vh] lg:pt-[10vh]'>
+        <section className='max-w-[1600px] mx-auto h-full rounded flex flex-col md:flex-row justify-between items-center'>
+                <section className='p-5 lg
+                :w-[40%] flex flex-col justify-center h-full place-items-center gap-3'>
+                    <p className='lg:text-3xl'>Get Up to 50% off on selected items</p>
+                    <button className='bg-green-600 text-white rounded-3xl px-4 py-2'>
+                        <p>See Deals</p>
+                    </button>
+                </section>
+                <Image src={HeroImg} width={500} height={500} alt={`ea`} className='lg:w-[60%] h-full rounded-r'/>
         </section>
     </main>
   )
