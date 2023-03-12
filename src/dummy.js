d030897faae993447a914076cde30c57651e7c21
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-03-12 11:13:25 +0000

    commit

---
+++ src/dummy.js
@@ -0,0 +1,91 @@
+export default 
+
+
+     [
+        {
+          name: "Super Ear Plugs",
+          description: "This is the first object",
+          price: 200,
+          img:'ear2.jpg'
+        },
+        {
+          name: "Airpods 2",
+          description: "This is the second object",
+          price: 340,
+          img:'airoids2.jpg'
+        },
+        {
+          name: "Airpods Pro",
+          description: "This is the third object",
+          price:459,
+          img:'airpodspro.jpg'
+        },
+        {
+          name: "Areari Ear Buds",
+          description: "This is the fourth object",
+          price:890,
+          img:'earbuds.jpg'
+        },
+        {
+          name: "JBL Headphones ",
+          description: "This is the fifth object",
+          price: 580,
+          img:'head2.jpg'
+        },
+        {
+          name: "Sony Superstar Head Phones",
+          description: "This is the sixth object",
+          price: 800,
+          img:'headphone.jpg'
+          
+        },
+        {
+          name: "Hunter Pro Headset ",
+          description: "This is the seventh object",
+          price: 1000,
+          img:'hunter.jpeg'
+
+        },
+        {
+          name: "Ipad Air 2",
+          description: "This is the eighth object",
+          price: 790,
+          img:'ipad air2.jpg'
+        },
+        {
+          name: "Ipad Pro",
+          description: "This is the ninth object",
+          price: 1230,
+          img:'ipadair.jpg'
+        },
+        {
+          name: "Object 10",
+          description: "This is the tenth object",
+          price: 980
+        },
+        {
+          name: "Object 11",
+          description: "This is the eleventh object",
+          price: 1400
+        },
+        {
+          name: "Object 12",
+          description: "This is the twelfth object",
+          price: 657
+        },
+        {
+          name: "Object 13",
+          description: "This is the thirteenth object",
+          price: 929
+        },
+        {
+          name: "Object 14",
+          description: "This is the fourteenth object",
+          price:2908
+        },
+        {
+          name: "Object 15",
+          description: "This is the fifteenth object",
+          price: 380
+        }
+      ];
