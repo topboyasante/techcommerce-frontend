994fcb9eb0ca6479e91f99adfa2042744ab9f86f
Nana K <73991180+topboyasante@users.noreply.github.com>
2023-03-14 17:27:22 +0000

    updates.

--- src/components/reusables/cartlogo.jsx
+++ src/components/reusables/cartlogo.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Badge from '@mui/material/Badge';
 import { styled } from '@mui/material/styles';
 import IconButton from '@mui/material/IconButton';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useSelector } from 'react-redux'
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
@@ -24,7 +24,7 @@ export default function CustomizedBadges() {
   return (
     <IconButton aria-label="cart">
       <StyledBadge badgeContent={quantity} color="secondary">
-        <ShoppingCartIcon />
+        {/* <ShoppingCartIcon /> */}
       </StyledBadge>
     </IconButton>
   );
