c44b92fb60abe1977b6e32d66ddafd4ebc28f76f
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-03-18 15:27:32 +0000

    Update [id].js

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useRouter } from 'next/router'
 import data from '../components/page-sections/dummy.js'
+import { useDispatch, useSelector } from 'react-redux'
+import {BiMinus,BiPlus} from 'react-icons/bi'
+import { cartActions } from '@/reduc/Cart.jsx'
 
 
 export default function DynamicR() {
     const router = useRouter()
    const itemid= router.query.id
+
    console.log(data[itemid])
+ 
+
 
    let prop= data[itemid]
 
+   const id= itemid;
+   const name= prop.name;
+   const price = prop.price;
+
+   console.log(itemid);
+
+   let [count,setcount]= useState(0);
+
+   
+
+const dispatch= useDispatch()
+
+   const additem=()=>{
+    
+    dispatch(cartActions.Add({
+      id,name,price
+      
+    }))
+    setcount(count++)
+  
+   }
+
+
+
+
   return (
-    <div className='pt-[16em] px-5'>
+    <div className='pt-[16em] px-5 w-full'>
   
     <div className=' mb-5'>
       <img src={`../images2/${prop.img}`} className='box w-full'/>
@@ -22,7 +53,22 @@ export default function DynamicR() {
       <div>{prop.description}</div>
       </div>
 
-      <div className='w-full'></div>    
+      <div className=' w-full border-b border-black my-2 lg:my-1'></div>
+
+      <div>
+        <p className=' font-bold text-xl my-[1em]'>Ghc <span>{prop.price}</span></p>
+
+        <div className='bord w-[8em] h-12 rounded-[2em]  flex justify-center items-center gap-4' >
+          <button>
+        <BiPlus size={20} className='' onClick={additem}/> 
+        </button>
+          <p className='text-2xl'>{count}</p>
+          <button>
+          <BiMinus size={20} className=''/>
+          </button>
+
+        </div>
+        </div>
     </div>
   )
 }
