5fe893a3b7220018fda4b3c82f6655ca302b059f
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-03-18 13:29:45 +0000

    dynamic page

--- src/pages/[id].js
+++ src/pages/[id].js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import data from '../dummy.js'
+import data from '../components/page-sections/dummy.js'
 
 
 export default function DynamicR() {
     const router = useRouter()
    const itemid= router.query.id
+   console.log(data[itemid])
+
+   let prop= data[itemid]
 
   return (
-    <div className='pt-[15em]'>Dynamic{itemid}</div>
+    <div className='pt-[16em] px-5'>
+  
+    <div className=' mb-5'>
+      <img src={`../images2/${prop.img}`} className='box w-full'/>
+      </div>
+<div  className=' flex flex-col gap-3'>
+      <div className=' text-3xl font-bold'>{prop.name}</div>
+
+      <div>{prop.description}</div>
+      </div>
+
+      <div className='w-full'></div>    
+    </div>
   )
 }
