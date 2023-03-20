374060c928c09b872f9ba2c6ff762375762716a3
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-03-20 23:28:14 +0000

    Update snackbar.jsx

--- src/components/reusables/snackbar.jsx
+++ src/components/reusables/snackbar.jsx
@@ -37,7 +37,7 @@ export default function SimpleSnackbar() {
 
   return (
     <div>
-      <Button onClick={handleClick}>Open simple snackbar</Button>
+      <Button onClick={handleClick}>Item Added to cart</Button>
       <Snackbar
         open={open}
         autoHideDuration={6000}
