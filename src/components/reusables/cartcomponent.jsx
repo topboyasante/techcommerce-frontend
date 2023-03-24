785d8badc7978a0106099fb8501126dad2226c72
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-03-24 10:53:11 -0700

    cartcomponent

---
+++ src/components/reusables/cartcomponent.jsx
@@ -0,0 +1,49 @@
+import { cartActions } from '@/reduc/Cart'
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import {BiMinus,BiPlus} from 'react-icons/bi'
+
+export default function Cartcomponent({id,name,price,quantity,img}) {
+    const dispatch = useDispatch()
+
+    const addButton= ()=>{
+        dispatch(cartActions.Add({id,name,img,price}))
+    }
+
+    
+    const delet = ()=>{
+        dispatch(cartActions.delete(id))
+    }
+  
+
+    return (
+  
+        <div className=' w-full p-1 px-2 rounded-lg flex items-center gap-2 scroll'>
+        <img src={`../images2/${img}`} className='w-[70px] h-[70px] object-cover rounded-[20px] scroll'/>
+        <div className=' flex flex-col gap-2'>
+         <p className=' text-base font-medium text-[white]'>
+             {name}
+         </p>
+         <p className='text-sm block text-white font-semibold'>
+             Ghc {price}
+         </p>
+         </div>
+         <div className='group flex items-center gap-2 ml-auto cursor-pointer'>
+             <button className=' border' onClick={addButton}>
+    
+                <BiPlus size={20} className=' text-gray-200'/> 
+    
+             </button>
+             <p className='w-5 h-5 pl-1 rounded-sm flex text-black items-center'>{quantity}</p>
+             <button className='border' onClick={delet}>
+                 <BiMinus size={20} className=' text-gray-200'/>
+             </button>
+         </div>
+         </div>
+    
+        
+    
+    
+        
+      )
+}
--- src/pages/cartpage.jsx
+++ src/pages/cartpage.jsx
@@ -1,19 +1,51 @@
+import Cartcomponent from '@/components/reusables/cartcomponent';
 import React from 'react'
 import { FaArrowLeft } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 export default function Cartpage() {
+
+    const cartList = useSelector((state)=> state.cart.Itemslist);
+    const data = cartList.map((item)=>{
+      return <Cartcomponent key={item.id} {...item} />
+
+    });
+    
+    //        <img src={empty} className=' h-[150px]' alt='empty cart'/>
+    
+
+    const auth = cartList === 0
   return (
-    <div className='w-full h-full flex justify-center items-center p-5'>
+    <div className='w-full h-full flex  justify-center items-center p-5'>
       
-        
       <div className='bord w-full min-h-[70vh] mt-[16em] '> 
-      <div className='w-full flex '>
+      <div className='w-full flex flex-col '>
      <p className='text-3xl font-semibold'> Your Cart</p>
+
+
+     <div className=' w-full h-full   rounded-t-[2rem] flex flex-col'>
+        <div className='bord w-full h-[340px] md:h-[500px] lg:h-[300px] md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll no-scrollbar'>
+        {!auth && <>  
+       {data}
+       </> }
+       {
+        auth && (
+            <div className=' flex flex-col items-center justify-evenly w-full h-full'>
+
+        <p className='text-white text-xl'>Empty Cart</p>
+        </div>
+        )
+       }
+   
+
+                
+
+    </div>
       
       </div>
 
 </div>
-
+</div>
     </div>
   )
 }
