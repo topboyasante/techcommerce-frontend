06fbba98ab288aeab0ba63ec6b9d011bce742d1e
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-03-24 11:09:55 -0700

    commit

--- src/components/reusables/cartcomponent.jsx
+++ src/components/reusables/cartcomponent.jsx
@@ -18,25 +18,25 @@ export default function Cartcomponent({id,name,price,quantity,img}) {
 
     return (
   
-        <div className=' w-full p-1 px-2 rounded-lg flex items-center gap-2 scroll'>
+        <div className=' w-full p-1 px-2 rounded-lg flex items-center bg-[#e0e0e0] gap-2 scroll'>
         <img src={`../images2/${img}`} className='w-[70px] h-[70px] object-cover rounded-[20px] scroll'/>
         <div className=' flex flex-col gap-2'>
-         <p className=' text-base font-medium text-[white]'>
+         <p className=' text-base font-medium text-black'>
              {name}
          </p>
-         <p className='text-sm block text-white font-semibold'>
+         <p className='text-sm block text-black font-semibold'>
              Ghc {price}
          </p>
          </div>
          <div className='group flex items-center gap-2 ml-auto cursor-pointer'>
-             <button className=' border' onClick={addButton}>
+             <button className=' border ' onClick={addButton}>
     
-                <BiPlus size={20} className=' text-gray-200'/> 
+                <BiPlus size={20} className=' text-black'/> 
     
              </button>
              <p className='w-5 h-5 pl-1 rounded-sm flex text-black items-center'>{quantity}</p>
              <button className='border' onClick={delet}>
-                 <BiMinus size={20} className=' text-gray-200'/>
+                 <BiMinus size={20} className=' text-black'/>
              </button>
          </div>
          </div>
--- src/pages/cartpage.jsx
+++ src/pages/cartpage.jsx
@@ -1,7 +1,7 @@
 import Cartcomponent from '@/components/reusables/cartcomponent';
 import React from 'react'
 import { FaArrowLeft } from 'react-icons/fa'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 export default function Cartpage() {
 
@@ -10,6 +10,11 @@ export default function Cartpage() {
       return <Cartcomponent key={item.id} {...item} />
 
     });
+
+    const total= 12;
+    const delivery= 23;
+    const finaltotal = 100
+
     
     //        <img src={empty} className=' h-[150px]' alt='empty cart'/>
     
@@ -18,13 +23,13 @@ export default function Cartpage() {
   return (
     <div className='w-full h-full flex  justify-center items-center p-5'>
       
-      <div className='bord w-full min-h-[70vh] mt-[16em] '> 
+      <div className=' w-full min-h-[70vh] mt-[16em] '> 
       <div className='w-full flex flex-col '>
      <p className='text-3xl font-semibold'> Your Cart</p>
 
 
      <div className=' w-full h-full   rounded-t-[2rem] flex flex-col'>
-        <div className='bord w-full h-[340px] md:h-[500px] lg:h-[300px] md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll no-scrollbar'>
+        <div className=' bg-[#f0f0f0] w-full h-[340px] md:h-[500px] lg:h-[300px] md:h-42 px-6 py-10 flex flex-col gap-3 overflow-y-scroll no-scrollbar'>
         {!auth && <>  
        {data}
        </> }
@@ -39,6 +44,57 @@ export default function Cartpage() {
    
 
                 
+
+    </div>
+    <div className=' w-full h-full  flex-1 bg-[#353535] rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2 text-white'>
+      <div className='w-full flex items-center justify-between'>
+      
+        <p className=' text-lg'>Subtotal</p>
+        <p className=' text-lg'>Ghc {total}.00</p>
+
+        
+
+      </div>
+      <div className='w-full flex items-center justify-between'>
+      
+      <p className=' text-lg'>Delivery</p>
+      <p className=' text-lg'>Ghc{delivery}.00</p>
+
+      
+
+    </div>
+
+ 
+
+    <div className=' w-full border-b border-white my-2 lg:my-1'></div>
+    <div className='w-full flex items-center justify-between'>
+      
+      <p className=' text-lg'>Total</p>
+      <p className=' text-lg'>Ghc {finaltotal}.00</p>
+
+    </div>
+{!renderingCheck &&
+  (    <button onClick={()=>{
+    add(finaltotal, cartList)
+    move('/thanks')
+
+    }} className= ' font-bold w-full h-[50px] lg:h-[35px] text-[black] rounded-[30px] bg-[#ccff01] hover:bg-[white] '>
+      Check Out
+
+</button>)
+}
+
+{
+  renderingCheck &&
+  (    <button onClick={()=>{
+
+    move('/thanks')}} className= ' font-bold w-full h-[50px] lg:h-[35px] text-[black] rounded-[30px] bg-[#ccff01] hover:bg-[white] '>
+      Check Out
+
+</button>)
+}
+ 
+
 
     </div>
       
